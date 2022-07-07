6618fb7ec0d3c6efa8166d16b64000f4cf829da4
pranabsarker <sarker.pranab@genweb2.com>
2022-07-08 01:35:19 +0600

    Company menu's pages created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,24 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {SapBusinessoneComponent} from "./products/sap-businessone/sap-businessone.component";
-import {SapBusinessBydesignComponent} from "./products/sap-business-bydesign/sap-business-bydesign.component";
-import {SapHanaComponent} from "./products/sap-hana/sap-hana.component";
-import {SapCustomerCheckoutComponent} from "./products/sap-customer-checkout/sap-customer-checkout.component";
-import {FocusPointComponent} from "./products/eCommerce/focus-point/focus-point.component";
-import {BigcommerceComponent} from "./products/eCommerce/bigcommerce/bigcommerce.component";
-import {SfaComponent} from "./products/sfa/sfa.component";
-import {SapApparelComponent} from "./products/sap-apparel/sap-apparel.component";
-import {AgriOneComponent} from "./products/agri-one/agri-one.component";
-import {TeaOneComponent} from "./products/tea-one/tea-one.component";
-import {LogisticOneComponent} from "./products/logistic-one/logistic-one.component";
-import {HrisComponent} from "./products/hris/hris.component";
+import {SapBusinessoneComponent} from "./company/products/sap-businessone/sap-businessone.component";
+import {SapBusinessBydesignComponent} from "./company/products/sap-business-bydesign/sap-business-bydesign.component";
+import {SapHanaComponent} from "./company/products/sap-hana/sap-hana.component";
+import {SapCustomerCheckoutComponent} from "./company/products/sap-customer-checkout/sap-customer-checkout.component";
+import {FocusPointComponent} from "./company/products/eCommerce/focus-point/focus-point.component";
+import {BigcommerceComponent} from "./company/products/eCommerce/bigcommerce/bigcommerce.component";
+import {SfaComponent} from "./company/products/sfa/sfa.component";
+import {SapApparelComponent} from "./company/products/sap-apparel/sap-apparel.component";
+import {AgriOneComponent} from "./company/products/agri-one/agri-one.component";
+import {TeaOneComponent} from "./company/products/tea-one/tea-one.component";
+import {LogisticOneComponent} from "./company/products/logistic-one/logistic-one.component";
+import {HrisComponent} from "./company/products/hris/hris.component";
 import {HomeComponent} from "./home/home.component";
 import {ContactUsComponent} from "./contact-us/contact-us.component";
+import {AwardsComponent} from "./company/awards/awards.component";
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {SuccessStoriesComponent} from "./company/success-stories/success-stories.component";
+import {ResourcesComponent} from "./company/resources/resources.component";
+import {PartnershipsComponent} from "./company/partnerships/partnerships.component";
 
 const routes: Routes = [
 
@@ -30,6 +35,11 @@ const routes: Routes = [
     {path: 'tea-one', component: TeaOneComponent},
     {path: 'logistic-one', component: LogisticOneComponent},
     {path: 'hris', component: HrisComponent},
+    {path: 'about-us', component: AboutUsComponent},
+    {path: 'awards', component: AwardsComponent},
+    {path: 'success-stories', component: SuccessStoriesComponent},
+    {path: 'resources', component: ResourcesComponent},
+    {path: 'partnerships', component: PartnershipsComponent},
     {path: 'contact-us', component: ContactUsComponent},
     {path: '**', redirectTo: ''}
 ];

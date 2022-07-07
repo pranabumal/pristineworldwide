fe08f46b69907d9ad68a411d37d75a80834254f3
pranabsarker <sarker.pranab@genweb2.com>
2022-07-08 01:27:35 +0600

    Product Section hubspot form added.

--- src/app/products/logistic-one/logistic-one.component.ts
+++ src/app/products/logistic-one/logistic-one.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare let hbspt: any
 
 @Component({
-  selector: 'app-logistic-one',
-  templateUrl: './logistic-one.component.html',
-  styleUrls: ['./logistic-one.component.scss']
+    selector: 'app-logistic-one',
+    templateUrl: './logistic-one.component.html',
+    styleUrls: ['./logistic-one.component.scss']
 })
 export class LogisticOneComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    ngAfterContentInit(): void {
+        hbspt.forms.create({
+            region: "na1",
+            portalId: "6764477",
+            formId: "49b8ec53-f4b6-4a73-aeb6-2381c9acdcfa",
+            target: "#hubspotForm"
+        });
+    }
 
 }
--- src/app/products/sfa/sfa.component.ts
+++ src/app/products/sfa/sfa.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare let hbspt: any
 
 @Component({
-  selector: 'app-sfa',
-  templateUrl: './sfa.component.html',
-  styleUrls: ['./sfa.component.scss']
+    selector: 'app-sfa',
+    templateUrl: './sfa.component.html',
+    styleUrls: ['./sfa.component.scss']
 })
 export class SfaComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    ngAfterContentInit(): void {
+        hbspt.forms.create({
+            region: "na1",
+            portalId: "6764477",
+            formId: "49b8ec53-f4b6-4a73-aeb6-2381c9acdcfa",
+            target: "#hubspotForm"
+        });
+    }
 }

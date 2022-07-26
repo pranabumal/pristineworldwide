f0b7e19185c5a72f77cf27af7586ccefc5f86bc2
pranab <sarker.pranab@genweb2.com>
2022-07-26 19:03:19 +0600

    company(partnership) page updated

--- src/app/company/partnerships/partnerships.component.ts
+++ src/app/company/partnerships/partnerships.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from "aos";
 
 @Component({
   selector: 'app-partnerships',
@@ -10,6 +11,7 @@ export class PartnershipsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+      AOS.init();
   }
 
 }

cb9877e5264c19bf7381ec3a84c9b7b00558273a
pranab <sarker.pranab@genweb2.com>
2022-07-26 17:10:50 +0600

    company(success-stories) page updated

--- src/app/company/success-stories/success-stories.component.ts
+++ src/app/company/success-stories/success-stories.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {OwlOptions} from "ngx-owl-carousel-o";
+
+import * as AOS from 'aos';
 
 @Component({
-  selector: 'app-success-stories',
-  templateUrl: './success-stories.component.html',
-  styleUrls: ['./success-stories.component.scss']
+    selector: 'app-success-stories',
+    templateUrl: './success-stories.component.html',
+    styleUrls: ['./success-stories.component.scss']
 })
 export class SuccessStoriesComponent implements OnInit {
 
-  constructor() { }
+    customOptions: OwlOptions = {
+        loop: true,
+        mouseDrag: true,
+        touchDrag: false,
+        pullDrag: false,
+        dots: false,
+        autoplay: true,
+        navText: ["<div class='nav-btn prev-slide'><img src='assets/images/icons/arrow-left.svg' alt=''></div>", "<div class='nav-btn next-slide'><img src='assets/images/icons/arrow-right.svg' alt=''></div>"],
+        responsive: {
+            0: {
+                items: 1
+            },
+            400: {
+                items: 2
+            },
+            740: {
+                items: 3
+            },
+            940: {
+                items: 3
+            }
+        },
+        nav: true
+    }
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void {
+        AOS.init();
+    }
 }

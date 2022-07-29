912d3dab06c2cbc74f0fbe40a43af0a04941597c
MD-Moinuddin <moinuddinmd956@gmail.com>
2022-07-29 17:17:59 +0600

    header (responsive update)

--- src/app/header-footer/header/header.component.ts
+++ src/app/header-footer/header/header.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+    isDropdownCollapsed: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+    handleDropDown(_default?: any) {
+        this.isDropdownCollapsed = !this.isDropdownCollapsed;
+    }
+
+    closeHamburgerMenu() {
+        this.isDropdownCollapsed = !this.isDropdownCollapsed;
+        document.getElementById('navbarsupportedcontent')?.classList.remove('show');
+    }
 }

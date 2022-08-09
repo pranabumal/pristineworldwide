c948f09429c74fa596e1bae2dc37ca8bc787565e
pranabsarker <sarker.pranab@genweb2.com>
2022-08-10 02:18:12 +0600

    14 Changes  updated

--- src/app/privacy-policy/privcy-policy.component.ts
+++ src/app/privacy-policy/privcy-policy.component.ts
@@ -16,15 +16,15 @@ export class PrivcyPolicyComponent implements OnInit {
         AOS.init();
     }
 
-    ngAfterContentInit(): void {
-        hbspt.forms.create({
-            region: "na1",
-            portalId: "22480607",
-            formId: "11b16cef-7bb3-4a36-b9af-2b44063de231",
-            target: "#hubspotForm"
-        });
-    }
-
+    // ngAfterContentInit(): void {
+    //     hbspt.forms.create({
+    //         region: "na1",
+    //         portalId: "22480607",
+    //         formId: "11b16cef-7bb3-4a36-b9af-2b44063de231",
+    //         target: "#hubspotForm"
+    //     });
+    // }
+    //
     scroll(el: any) {
         el.scrollIntoView();
     }

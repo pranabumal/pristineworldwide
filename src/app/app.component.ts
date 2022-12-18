ee0a627f1634e41e290b43f564a0a2feff7512a8
pranab <sarker.pranab@genweb2.com>
2022-12-18 16:30:24 +0600

    Site Map for google analytics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import {NavigationEnd, Router} from "@angular/router";
+// tslint:disable-next-line:ban-types
+declare let gtag: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'App Started';
+    title = 'Pristine Solutions';
+    constructor(public router: Router){
+        this.router.events.subscribe(event => {
+                if(event instanceof NavigationEnd){
+                    gtag('config', 'G-DNPNS9039W',
+                        {
+                            'page_path': event.urlAfterRedirects
+                        }
+                    );
+                }
+            }
+        )}
 }
